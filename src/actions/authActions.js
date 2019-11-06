c5034bc8c5bf9a42371d2a48762e7c4013315763
jun-low <zhijun.low@gmail.com>
2019-11-06 15:31:50 +0800

    Update authActions.js

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -11,7 +11,7 @@ import {
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post("http://localhost:4000/users/register", userData)
-    .then(res => history.push("/users/login")) // re-direct to login on successful register
+    .then(res => history.push("/"))
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
@@ -24,15 +24,12 @@ export const loginUser = userData => dispatch => {
   axios
     .post("http://localhost:4000/users/login", userData)
     .then(res => {
-      // Save to localStorage
-// Set token to localStorage
+
       const { token } = res.data;
       localStorage.setItem("jwtToken", token);
-      // Set token to Auth header
       setAuthToken(token);
-      // Decode token to get user data
+
       const decoded = jwt_decode(token);
-      // Set current user
       dispatch(setCurrentUser(decoded));
     })
     .catch(err =>
@@ -42,25 +39,22 @@ export const loginUser = userData => dispatch => {
       })
     );
 };
-// Set logged in user
+
 export const setCurrentUser = decoded => {
   return {
     type: SET_CURRENT_USER,
     payload: decoded
   };
 };
-// User loading
+
 export const setUserLoading = () => {
   return {
     type: USER_LOADING
   };
 };
-// Log user out
+
 export const logoutUser = () => dispatch => {
-  // Remove token from local storage
   localStorage.removeItem("jwtToken");
-  // Remove auth header for future requests
   setAuthToken(false);
-  // Set current user to empty object {} which will set isAuthenticated to false
   dispatch(setCurrentUser({}));
 };
